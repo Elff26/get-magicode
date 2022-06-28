e7c1129d05d675dd975ab36693e155c837c36e2a
lais.farias <lais.farias@INOTE842>
2022-06-28 16:11:32 -0300

    feat(navigator): Add parametros para header button(ButtonReturn) nas telas Login, Register e Home

--- src/navigator.js
+++ src/navigator.js
@@ -1,10 +1,12 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 import Home from './pages/HomeScreen/home';
 import RegisterScreen from './pages/RegisterScreen/Register'
 import Login from './pages/LoginScreen/login'
 import ChooseTechnologias from './pages/ChooseTechnologiesScreen/chooseTechnologies';
+import ButtonReturn from './components/Buttons/ButtonReturn';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,11 +14,43 @@ const Navigator = (
     <NavigationContainer>
         <Stack.Navigator 
             initialRouteName="Home"             
-            screenOptions={{ headerShown: false }}
+            screenOptions={{ headerShown: true }}
         >
-            <Stack.Screen name='Home' component={Home} />
-            <Stack.Screen name='Login' component={Login} />
-            <Stack.Screen name='Register' component={RegisterScreen} />
+            <Stack.Screen 
+                name='Home' 
+                component={Home}
+                options={{title:''}} 
+            />
+            <Stack.Screen 
+                name='Login' 
+                component={Login} 
+                options={(props) =>({
+                    title: '',
+                    headerLeft: () => 
+                        <HeaderButtons HeaderButtonComponent={ButtonReturn}>
+                            <Item 
+                                title='Voltar'
+                                iconName='chevron-back-outline'
+                                onPress={() => {props.navigation.navigate('Home')}}
+                            />
+                        </HeaderButtons>
+                })} 
+            />
+            <Stack.Screen 
+                name='Register' 
+                component={RegisterScreen}
+                options={(props) =>({
+                    title: '',
+                    headerLeft: () => 
+                        <HeaderButtons HeaderButtonComponent={ButtonReturn}>
+                            <Item 
+                                title='Voltar'
+                                iconName='chevron-back-outline'
+                                onPress={() => {props.navigation.navigate('Home')}}
+                            />
+                        </HeaderButtons>
+                })} 
+            />
             <Stack.Screen name='ChooseTechnologies' component={ChooseTechnologias} />
         </Stack.Navigator>
     </NavigationContainer>
