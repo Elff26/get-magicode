bf626b29f32dea6c28149d59a926d40fa2441658
lais.farias <lais.farias@INOTE842>
2022-06-29 12:28:17 -0300

    feat(ForgotPasswordRecovery): Ajustando estilização e identação

--- src/pages/ForgotPassword/ForgotPasswordRecovery.js
+++ src/pages/ForgotPassword/ForgotPasswordRecovery.js
@@ -5,7 +5,7 @@ import ButtonComponent from '../../components/Buttons/ButtonComponent';
 import Colors from "../../components/ColorPallete/Colors";
 import Header from '../../components/Header/HeaderComponent';
 
-export default function ForgotPasswordEmail() {
+export default function ForgotPasswordEmail({navigation}) {
     const [PasswordRecovery, setPasswordRecovery] = useState("");
 
     function BackToLogin() {
@@ -14,60 +14,53 @@ export default function ForgotPasswordEmail() {
 
     return ( 
         <View style={styles.allPagesRecovery}>
+            <Header backArrow={true} navigation={navigation} />
             <View style={styles.screenContainerRecovery}>
-
-                    {/* TO DO */}
-                    {/* <Header backArrow={true} navigation={navigation} /> */}
-
-                        <View>
-                            <Text style={styles.titleRecovery}>Recuperação de Senha</Text>
-                        </View>
-
-
-                        <View style={styles.formRecovery}>
-
-                                <Text style={styles.descriptionRecovery}>Informe sua Nova Senha:</Text>
-                            
-                                <TextInput
-                                value={PasswordRecovery}
-                                placeholder= "Senha"
-                                onChangeText= {setPasswordRecovery}
-                                style={styles.textInputRecovery}
-                                />
-
-                            <View>
-                                <Text style={styles.descriptionRecovery}>Confirme sua Nova Senha:</Text>
-                            </View>
-
-                                <TextInput
-                                value={PasswordRecovery}
-                                placeholder= "Senha"
-                                onChangeText= {setPasswordRecovery}
-                                style={styles.textInputRecovery}
-                                />
-
-                            <ButtonComponent onPress={BackToLogin}>
-                                <Text style={styles.textAgreeButton}>Confirmar</Text>
-                            </ButtonComponent>
-
-                        </View>
+                <View>
+                    <Text style={styles.titleRecovery}>Recuperação de senha</Text>
+                </View>
+                <View style={styles.formRecovery}>
+                    <Text style={styles.descriptionRecovery}>Informe sua nova senha:</Text>
+                    <TextInput
+                    value={PasswordRecovery}
+                    placeholder= "Senha"
+                    onChangeText= {setPasswordRecovery}
+                    style={styles.textInputRecovery}
+                    />
+                    <View>
+                        <Text style={styles.descriptionRecovery}>Confirme sua nova senha:</Text>
+                    </View>
+                    <TextInput
+                    value={PasswordRecovery}
+                    placeholder= "Senha"
+                    onChangeText= {setPasswordRecovery}
+                    style={styles.textInputRecovery}
+                    />
+                    <ButtonComponent newStyle={styles.buttonRecovery} onPress={BackToLogin}>
+                        <Text style={styles.textAgreeButton}>Confirmar</Text>
+                    </ButtonComponent>
+                </View>
             </View>
         </View>   
     );
 }
 
 const styles = StyleSheet.create({
+    allPagesRecovery:{
+        flex: 1,
+        backgroundColor: '#fff'
+   },
+   formRecovery:{
+        marginTop: 100,
+        justifyContent: 'center',
+        textAlign: 'center',
+        alignItems: 'center'
+    },
     titleRecovery: {
         color: '#33B9D4',
         fontSize: '30px',
         textAlign: 'center'
     },
-    formRecovery:{
-        marginTop: 100,
-        justifyContent: 'center',
-        textAlign: 'center',
-        alignItems: 'center',
-   },
    textInputRecovery:{
         width: 304,
         height: 40,
@@ -78,25 +71,21 @@ const styles = StyleSheet.create({
         borderRadius: 20,
         textAlign: 'center',
         justifyContent: 'center',
-        marginBottom: '30px',
-        marginTop: '30px',
+        margin: 10
    },
    textAgreeButton:{
         color:'#FFFFFF'
    },
    buttonRecovery: {
-        width: 158,
+        width: 198,
         height: 49,
         borderRadius: 20,
+        marginTop: 10
    },
    descriptionRecovery:{
         textAlign:"center", 
         marginTop:10, 
         color:Colors.TEXT_COLOR, 
         fontSize:24
-   },
-   allPagesRecovery:{
-        flex: 1,
-        backgroundColor: '#fff'
    }
 });
